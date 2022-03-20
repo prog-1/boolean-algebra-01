2b01fd73d80264ead07b2972a7d24baac56005d7
Андрей Черемных <andrejceremnih@MacBook-Air-Andrej.local>
2022-03-20 22:43:13 +0200

    added small changes

--- main.go
+++ main.go
@@ -12,12 +12,12 @@ func proposition(a, b, c, d bool) bool {
 
 func main() {
 	bools := [2]bool{false, true}
-	fmt.Println("    A      B      C      D    (A => B || C) && (!B => A && D) && (D => B != C)")
+	fmt.Println("    A      B      C      D    (A => (B || C)) && (!B => (A && D)) && (D => (B != C))")
 	for _, a := range bools {
 		for _, b := range bools {
 			for _, c := range bools {
 				for _, d := range bools {
-					fmt.Printf("%7v%7v%7v%7v%28v\n", a, b, c, d, proposition(a, b, c, d))
+					fmt.Printf("%7v%7v%7v%7v%30v\n", a, b, c, d, proposition(a, b, c, d))
 				}
 			}
 		}
